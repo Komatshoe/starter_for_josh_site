f2cb296e0d2e4bf67da3ec70016573ea7e9c7f58
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-04-17 00:03:00 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -34,24 +34,31 @@
           </div>
       
           <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
-              <h4><?php the_field('articletitle'); ?></h4>
-              <p><?php the_field('articleexcerpt'); ?></p>
-              <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage1'); ?>">
+              <h4><?php the_field('articletitle1'); ?></h4>
+              <p><?php the_field('articleexcerpt1'); ?></p>
+              <a href="<?php the_field('readmore1'); ?>" class="readmore">CONTINUE READING</a>
 <!--placeholder for col1-->
 
           </div>
               
               
               <div class="col-md-4 article-cent">
-
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage2'); ?>">
+              <h4><?php the_field('articletitle2'); ?></h4>
+              <p><?php the_field('articleexcerpt2'); ?></p>
+              <a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
 <!--placeholder for col1-->
                   
           </div>
               
               
               <div class="col-md-4 article-cent">
-                                 
+                          <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage3'); ?>">
+              <h4><?php the_field('articletitle3'); ?></h4>
+              <p><?php the_field('articleexcerpt3'); ?></p>
+              <a href="<?php the_field('readmore3'); ?>" class="readmore">CONTINUE READING</a>
+<!--placeholder for col1-->       
 <!--placeholder for col1-->
                   
           </div>
